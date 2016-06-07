e810d4d16467488b3d525fb474b87f7a615fc44d
stanleynguyen <hung.ngn.the@gmail.com>
2016-06-07 16:11:26 +0000

    search result page

--- app/engine.js
+++ app/engine.js
@@ -2,12 +2,13 @@ var Yelp = require("yelp");
 var yelp = new Yelp(require('../config/yelp'));
 
 module.exports.yelpSearch = function(req, res){
-    console.log(req.body)
     var term = req.body.keyword;
     var location = req.body.location;
-    // var offset = req.body.offset || 0;
-    yelp.search({term: term, location: location/*, limit: 10, offset: offset*/}, function(err, data){
+    yelp.search({term: term, location: location}, function(err, data){
         if (err) console.log(err);
-        res.json(data);
+        res.render('search.ejs',{
+            search: req.body,
+            results: data.businesses
+        });
     });
 };
