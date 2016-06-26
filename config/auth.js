10984f488abb6aa3abaed294fc3ba0c1888e2b9a
stanleynguyen <hung.ngn.the@gmail.com>
2016-06-26 14:46:18 +0000

    change to production call back url

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     facebook:{
         clientID: '780801485388556',
         clientSecret: '33287d882fc82f6cc4455c457a710aeb',
-        callbackURL: 'http://free-code-camp-stanleynguyen.c9users.io/auth/facebook/callback',
+        callbackURL: 'http://webderlust.herokuapp.com/auth/facebook/callback',
         profileFields: ['id', 'displayName', 'link', 'emails']
     }
 };
